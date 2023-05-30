6ce41535a6aec33619e9c32b0285c3ceb5298d0e
Leo <Leo@DELL_PC>
2023-05-30 08:58:53 -0300

    Initial commit

---
+++ GeekShopping/GeekShopping.ProductAPI/Mappings/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+
+namespace GeekShopping.ProductAPI.Mappings
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<ProductVO, Product>();
+            CreateMap<Product, ProductVO>();
+        }
+    }
+}
---
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,51 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Mappings;
+using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+builder.Services.AddDbContextFactory<MySQLContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("MySQLConnectionString"), new MySqlServerVersion(new Version(8,0,33)));
+});
+
+// Services
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+});
+
+// Add a mapper region
+var config = new MapperConfiguration(cfg =>
+{
+    cfg.AddProfile(new AutoMapperProfile());
+});
+var mapper = config.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
